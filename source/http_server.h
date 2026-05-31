f0993b31abdb4e26003e07b03196c3965475b214
gmaitxqqq <42579302+gmaitxqqq@users.noreply.github.com>
2026-05-31 18:08:35 +0800

    feat: add source/http_server.h

---
+++ source/http_server.h
@@ -0,0 +1,16 @@
+/**
+ * http_server.h - Lightweight HTTP server for pctltcp-web
+ */
+
+#ifndef HTTP_SERVER_H
+#define HTTP_SERVER_H
+
+#include <switch.h>
+
+#define HTTP_PORT  8081
+
+void http_server_start(void);
+void http_server_stop(void);
+bool http_server_is_running(void);
+
+#endif /* HTTP_SERVER_H */
